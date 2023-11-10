b4c2520176966dcfec56c18c274bc86b5f3ec18d
gabisoaresm <119060418+gabisoaresm@users.noreply.github.com>
2023-11-10 19:37:04 -0300

    Add files via upload

---
+++ tarefa.py
@@ -0,0 +1,39 @@
+#Lista de tarefas 
+#Importar módulo reutilizável de listas
+#Cada elemento da lista é uma lista que contém a tarefa, seu tempo de duração e seus
+#pré-requisitos, respectivamente
+
+def criar_lista():
+    lista = []
+    return lista
+
+def adicionar_elemento(lista, elemento):
+    lista.append(elemento)
+    return
+
+def remover_elemento(lista, elemento):
+    lista.remove(elemento)
+    return
+
+#Funções criar_lista, adicionar_elemento e remover_elemento fazem parte do módulo
+#que será importado
+
+def create_task(tarefa, list_tarefas):
+    adicionar_elemento(list_tarefas, tarefa)
+    return 
+
+def remove_task(tarefa, list_tarefas):
+    remover_elemento(list_tarefas, tarefa)
+    return 
+
+def set_duration(tarefa, tempo, list_tarefas):
+    #adiciona, para cada tarefa, seu tempo de duração
+    
+    return list_tarefas #retorna lista atualizada com o tempo de duração de cada tarefa
+
+
+def update_prereqs(tarefa, prereq, list_tarefas):
+    #adiciona, para cada tarefa, seus pré-requisitos
+    
+    return list_tarefas #retorna lista atualizada com os pré-requisitos de cada tarefa
+

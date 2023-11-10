cf57e748c974324f3098bf491c17ab1724592a07
Rodrigosnrocha <rodrigosnr@al.insper.edu.br>
2023-11-10 20:48:29 -0300

    Complementar esqueleto path.py

--- path.py
+++ path.py
@@ -1,11 +1,17 @@
+#TODO
+#Testes de API
+#Implementar modulo de listas
+#O caminho critico pode ser gerado calculando todas as listas de antecessoras a partir do final do projeto e escolhendo a maior
 
 
-
-def set_dates():
+# Definir datas de início e fim para uma tarefa, a partir de seu início estimado e duração
+def set_dates(tarefa):
     return 0
 
-def find_successor():
+# Encontra a tarefa sucessora, a partir de uma tarefa e a lista de todas as tarefas
+def find_successor(tarefa, projeto):
     return 0
 
-def critical_path():
+# Gera o caminho crítico do projeto a partir de todas as tarefas
+def critical_path(projeto):
     return 0

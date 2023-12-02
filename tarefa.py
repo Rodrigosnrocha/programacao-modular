a6cb5b41732ba1c5374c054d5740f7e896385bd9
BrunoP0019 <149721810+BrunoP0019@users.noreply.github.com>
2023-12-02 18:49:39 -0300

    Update tarefa.py

--- tarefa.py
+++ tarefa.py
@@ -14,9 +14,18 @@ def adicionar_elemento(lista, elemento):
     lista.append(elemento)
     return
 
-def remover_elemento(lista, elemento):
-    lista.remove(elemento)
-    return
+def remove_task(tarefa, lista_tarefas):
+    lista_copia = lista_tarefas.copy()  # Crie uma cópia da lista
+    
+    for el in lista_copia:
+        if type(el[2]) == list and tarefa[0] in el[2]:
+             remover_elemento(el[2], tarefa[0])
+        elif el[2] == tarefa[0]:
+            el[2] = ''
+    
+    remover_elemento(lista_copia, tarefa)
+     # Remova a tarefa da cópia
+    return lista_copia
 
 #Funções criar_lista, adicionar_elemento e remover_elemento fazem parte do módulo que será importado
 #Criar list_tarefas no programa principal com a função criar_lista do módulo reutilizavel

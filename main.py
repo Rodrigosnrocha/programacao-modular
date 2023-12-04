737d1c53d60e1553ae16a86e65f54ba681c00576
Rodrigosnrocha <rodrigosnr@al.insper.edu.br>
2023-12-04 17:39:10 -0300

    Criado programa main

--- main.py
+++ main.py
@@ -2,3 +2,27 @@ import tarefa, path
 import Modulo_reutilizavel_lista as lista
 
 listaTarefas = lista.criar_lista()
+
+tarefa.create_task("Criar esqueleto", listaTarefas)
+tarefa.set_duration("Criar esqueleto", 2, listaTarefas)
+
+tarefa.create_task("Criar testes de API", listaTarefas)
+tarefa.set_duration("Criar testes de API", 1, listaTarefas)
+
+tarefa.create_task("Implementar func 1", listaTarefas)
+tarefa.set_duration("Criar testes de API", 3, listaTarefas)
+tarefa.update_prereqs("Implementar func 1", "Criar esqueleto", listaTarefas)
+tarefa.update_prereqs("Implementar func 1", "Criar testes de API", listaTarefas)
+
+tarefa.create_task("Implementar func 2", listaTarefas)
+tarefa.set_duration("Criar testes de API", 5, listaTarefas)
+
+tarefa.create_task("Implementar func 3", listaTarefas)
+tarefa.set_duration("Criar testes de API", 7, listaTarefas)
+tarefa.update_prereqs("Implementar func 3", "Implementar func 1", listaTarefas)
+
+path.set_dates(listaTarefas, "01/12", 2023)
+
+caminhoCritico = path.critical_path(listaTarefas)
+
+print(caminhoCritico)

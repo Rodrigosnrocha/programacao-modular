fbb51977c55f99fe9dec7a933e2082a66e5d369b
Rodrigosnrocha <rodrigosnr@al.insper.edu.br>
2023-12-04 19:15:31 -0300

    Atualizada main

--- main.py
+++ main.py
@@ -10,23 +10,25 @@ tarefa.create_task("Criar testes de API", listaTarefas)
 tarefa.set_duration("Criar testes de API", 1, listaTarefas)
 
 tarefa.create_task("Implementar func 1", listaTarefas)
-tarefa.set_duration("Criar testes de API", 3, listaTarefas)
+tarefa.set_duration("Implementar func 1", 3, listaTarefas)
 tarefa.update_prereqs("Implementar func 1", "Criar esqueleto", listaTarefas)
 tarefa.update_prereqs("Implementar func 1", "Criar testes de API", listaTarefas)
 
 tarefa.create_task("Implementar func 2", listaTarefas)
-tarefa.set_duration("Criar testes de API", 5, listaTarefas)
+tarefa.set_duration("Implementar func 2", 5, listaTarefas)
 
 tarefa.create_task("Implementar func 3", listaTarefas)
-tarefa.set_duration("Criar testes de API", 7, listaTarefas)
+tarefa.set_duration("Implementar func 3", 7, listaTarefas)
 tarefa.update_prereqs("Implementar func 3", "Implementar func 1", listaTarefas)
 
 path.set_dates(listaTarefas, "01/12", 2023)
 
 caminhoCritico = path.critical_path(listaTarefas)
 
-texto = f"Caminho crítico: {caminhoCritico[0][0]}"
+print(f"Data início {caminhoCritico[0][3][0]} - Data fim {caminhoCritico[-1][3][1]}")
+print(f"Caminho crítico: ")
+texto = ""
 for i in caminhoCritico:
-    texto +=f", {i[0]}"
+    texto +=f"|{i[0]} - Início {i[3][0]} - Fim {i[3][1]}| "
 
 print(texto)
